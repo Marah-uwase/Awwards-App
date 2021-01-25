b1c86041e34683f0193b31d09b279616fc4d00c4
Marah-uwase <maranahuwase12@gmail.com>
2021-01-25 12:27:01 +0200

    adding the urlconf for the api profile

--- awward/urls.py
+++ awward/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     
     url(r'^$', views.home_projects, name='homePage'),
     url(r'^ajax/newsletter/$', views.newsletter, name='newsletter'),
-    url(r'^api/profile/$', views.ProfileList.as_view()),
+    url(r'^api/profile/$', views.ProfileList.as_view())
     url(r'^edit/profile$', views.edit_profile, name='edit_profile'),
     url(r'^profile/(?P<username>[0-9]+)$',
         views.individual_profile_page, name='individual_profile_page'),
